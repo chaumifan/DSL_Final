484e1c1a3127d48af6b5e4819ecdfd0e333df230
ChauMiFan <kevinchau@utexas.edu>
2017-12-05 23:49:33 -0600

    Followed mido/examples/midifiles

--- midi_sample.py
+++ midi_sample.py
@@ -1,11 +1,18 @@
+import random
 from mido import Message, MidiFile, MidiTrack
+
+notes = [64, 64+7, 64+12]
 
 mid = MidiFile()
 track = MidiTrack()
 mid.tracks.append(track)
 
 track.append(Message("program_change", program=12, time=0))
-track.append(Message("note_on", note=64, velocity=64, time=32))
-track.append(Message("note_off", note=64, velocity=127, time=32))
+
+delta = 16
+for i in range(100):
+    note = random.choice(notes)
+    track.append(Message("note_on", note=note, velocity=100, time=delta))
+    track.append(Message("note_off", note=note, velocity=100, time=delta))
 
 mid.save("new_song.mid")
