14783fd1e1a7e2c3cc3c624bbad6534695d8f513
Kevin Tai <kevin.r.tai@gmail.com>
2017-12-09 15:51:38 -0600

    Midi segment converter

--- src/preprocessing/split_midi.py
+++ src/preprocessing/split_midi.py
@@ -1,21 +1,21 @@
 import mido
 from mido import MidiFile, MidiTrack, Message, MetaMessage
+from os import listdir
+from os.path import isfile, split, join
 
-def main():
-  mid_file = '../data/midi/daylight.mid'
-  song_name = 'test/daylight'
-
+def split_midi(mid_file, target_dir, default_tempo=500000, target_segment_len=1):
+  '''Split midi file into many chunks'''
+  song_name = split(mid_file)[-1][:-4]
   mid = MidiFile(mid_file)
-  
+
   # identify the meta messages
   metas = []
-  tempo = 500000
+  tempo = default_tempo
   for msg in mid:
     if msg.type is 'set_tempo':
       tempo = msg.tempo
     if msg.is_meta:
       metas.append(msg)
-
   for meta in metas:
     meta.time = int(mido.second2tick(meta.time, mid.ticks_per_beat, tempo))
 
@@ -36,11 +36,9 @@ def main():
     if msg.type is not 'end_of_track':
       msg.time = int(mido.second2tick(msg.time, mid.ticks_per_beat, tempo))
       track.append(msg)
-      print(msg)
     if msg.type is 'end_of_track' or time_elapsed >= target_segment_len:
-      print('----------------')
       track.append(MetaMessage('end_of_track'))
-      target.save(song_name + '_{}.mid'.format(prefix))
+      target.save(join(target_dir, song_name + '_{}.mid'.format(prefix)))
       target = MidiFile()
       track = MidiTrack()
       track.extend(metas)
@@ -48,6 +46,26 @@ def main():
       time_elapsed = 0
       prefix += 1
 
+
+def parse_args():
+  '''Parse arguments'''
+  parser = argparse.ArgumentParser()
+  parser.add_argument('input_dir', type=str)
+  parser.add_argument('target_dir', type=str)
+  return parser.parse_args()
+
+
+def main():
+  args = parse_args()
+  input_dir = args.input_dir
+  target_dir = args.target_dir
+
+  # Get all the input midi files
+  midis = [x for x in listdir(input_dir) if x.endswith('.mid')]
+
+  for midi in midis:
+    split_midi(midi, target_dir)
+  
 
 if __name__ == '__main__':
   main()
